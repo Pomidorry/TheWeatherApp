bd5c43622f09a3af8a89ab8d24fd9db39ed43261
Pomidorry <marymarusya0@gmail.com>
2024-07-29 17:45:08 +0300

    Integrated API interraction

--- WeatherTracker/force-app/main/default/lwc/weatherApp/weatherApp.js
+++ WeatherTracker/force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -1,3 +1,24 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import retriveWeather from '@salesforce/apex/WeatherController.retriveWeather'
 
-export default class WeatherApp extends LightningElement {}
+export default class WeatherApp extends LightningElement {
+    @track cityName;
+    @track weatherData;
+
+    handleCityChange(event){
+        this.cityName = event.target.value;
+        console.log(this.cityName);
+    }
+
+    getWeather() {
+        retriveWeather({cityName: this.cityName})
+            .then(result => {
+                let date = result.weather[0].date.toString();
+                this.weatherData = date;
+                console.log(date);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+}
