cd9b023e6dac9327601d28aa6b860f7dbad1ae0a
Pomidorry <marymarusya0@gmail.com>
2024-07-30 17:41:36 +0300

    Exceptions handled

--- WeatherTracker/force-app/main/default/lwc/weatherApp/weatherApp.js
+++ WeatherTracker/force-app/main/default/lwc/weatherApp/weatherApp.js
@@ -2,26 +2,35 @@ import { LightningElement, track } from 'lwc';
 import retriveWeather from '@salesforce/apex/WeatherController.retriveWeather'
 
 export default class WeatherApp extends LightningElement {
-    @track cityName;
+    @track cityName="";
     @track weatherData;
     @track temperature;
     @track windspeed;
     @track weatherDesc;
+    @track error;
 
     handleCityChange(event){
         this.cityName = event.target.value;
         console.log(this.cityName);
     }
 
-    getWeatherData() {
-        retriveWeather({cityName: this.cityName})
-            .then(result => {
-                this.weatherData = true;
-                this.formatWeatherData(result);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    async getWeatherData() {
+        if (this.cityName === "") {
+            this.weatherData = false;
+            this.error = "Please enter city name";
+        }
+        else{
+            retriveWeather({cityName: this.cityName})
+                .then(result => {
+                    this.weatherData = true;
+                    this.formatWeatherData(result);
+                })
+                .catch(error => {
+                    this.weatherData = false;
+                    this.error = "Please enter VALID city name";
+                    console.log(error);
+                });
+        }
     }
     formatWeatherData(res) {
         this.temperature = res.weather[0].avgtempC.toString();
